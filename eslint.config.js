4f0091e218f3fd4775fc09fa1fd7de2d494ab093
Kashif Rezwi <kashifrezwi850@gmail.com>
2026-08-28 23:54:47 +0530

    refactor: remove example code and integration test fixtures

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import prettier from "eslint-config-prettier";
 
 export default tseslint.config(
   {
-    ignores: ["dist", "coverage", "node_modules", "examples/**/dist"],
+    ignores: ["dist", "coverage", "node_modules"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
